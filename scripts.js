f718a2fbc10411777cc21c00d81a496d15375b98
jhon Cavalli <jhoncavalli@hotmail.com>
2025-06-28 14:03:44 -0300

    Adicionar variável para selecionar a máscara do modal e garantir visibilidade ao exibir

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 
 
 const modal = document.querySelector('.modal');
-
+const mascara = document.querySelector('.mascara-modal');
 
 
 function mostrarModal() {
     modal.style.left = '50%';
+    modal.style.visibility = 'visible';
 }
